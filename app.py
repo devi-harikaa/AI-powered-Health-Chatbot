d3d0ba225b0bdc70994ab04e5ea205147d978fbe
Devi Harika <harikadevi414@gmail.com>
2025-02-10 13:19:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ st.title("AI Chatbot 🤖 (Powered by Google Gemini)")
 st.write("Ask me anything!")
 
 # Load API key (from Streamlit secrets)
-genai.configure(api_key=st.secrets["GOOGLE_GEMINI_API_KEY"])
+genai.configure(api_key=st.secrets["AIzaSyB5mtrw4UbGBw3VwjKfKZgnfyLZ6BbGiFM"])
 
 # Initialize chat history
 if "messages" not in st.session_state:
